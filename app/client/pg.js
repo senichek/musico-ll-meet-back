fdff417dcd94fb990622254ae492d2230119681e
senichek <senichek@gmail.com>
2022-11-05 20:35:26 +0100

    changed pg config

--- app/client/pg.js
+++ app/client/pg.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     //ssl: false,
-    ssl: {
+    /* ssl: {
         rejectUnauthorized: false,
-    },
+    }, */
 });
 
 client.connect();
